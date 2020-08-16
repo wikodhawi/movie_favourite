eb330e203e0a6a450921e042ae9944caf70a32e6
Dhaba Widhikari <wikodhawi@gmail.com>
2020-08-16 19:18:47 +0700

    top rated movie

--- app/src/test/java/com/stickearn/moviefavourite/api/ApiTest.kt
+++ app/src/test/java/com/stickearn/moviefavourite/api/ApiTest.kt
@@ -79,6 +79,36 @@ class ApiTest {
         }
     }
 
+    @Test
+    fun getPopularMovie(){
+        runBlocking {
+            enqueueResponse("popular_movie.json")
+            val resultResponse = service.getPopularMovie(BuildConfig.API_KEY)
+            val selectedMovie = resultResponse.results[0]
+            Assert.assertThat(selectedMovie.id, CoreMatchers.`is`(612706))
+            Assert.assertThat(selectedMovie.releaseDate, CoreMatchers.`is`("2020-08-07"))
+            Assert.assertThat(selectedMovie.backdropPath, CoreMatchers.`is`("/ishzDCZIv9iWfI70nv5E4ZreYUD.jpg"))
+            Assert.assertThat(selectedMovie.posterPath, CoreMatchers.`is`("/b5XfICAvUe8beWExBz97i0Qw4Qh.jpg"))
+            Assert.assertThat(selectedMovie.overview, CoreMatchers.`is`("A brilliant but clumsy high school senior vows to get into her late father's alma mater by transforming herself and a misfit squad into dance champions."))
+            Assert.assertThat(selectedMovie.title, CoreMatchers.`is`("Work It"))
+        }
+    }
+
+    @Test
+    fun getTopRated(){
+        runBlocking {
+            enqueueResponse("top_rated_movie.json")
+            val resultResponse = service.getTopRatedMovie(BuildConfig.API_KEY)
+            val selectedMovie = resultResponse.results[0]
+            Assert.assertThat(selectedMovie.id, CoreMatchers.`is`(724089))
+            Assert.assertThat(selectedMovie.releaseDate, CoreMatchers.`is`("2020-07-31"))
+            Assert.assertThat(selectedMovie.backdropPath, CoreMatchers.`is`("/jtAI6OJIWLWiRItNSZoWjrsUtmi.jpg"))
+            Assert.assertThat(selectedMovie.posterPath, CoreMatchers.`is`("/pci1ArYW7oJ2eyTo2NMYEKHHiCP.jpg"))
+            Assert.assertThat(selectedMovie.overview, CoreMatchers.`is`("Professor Gabriel Emerson finally learns the truth about Julia Mitchell's identity, but his realization comes a moment too late. Julia is done waiting for the well-respected Dante specialist to remember her and wants nothing more to do with him. Can Gabriel win back her heart before she finds love in another's arms?"))
+            Assert.assertThat(selectedMovie.title, CoreMatchers.`is`("Gabriel's Inferno Part II"))
+        }
+    }
+
     private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
         val inputStream = javaClass.classLoader!!
             .getResourceAsStream("api-response/$fileName")

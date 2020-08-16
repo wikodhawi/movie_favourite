3920b1a0eb968874476c0302bd5cc10fddde9386
Dhaba Widhikari <wikodhawi@gmail.com>
2020-08-16 20:11:42 +0700

    set data to db and restore it

---
+++ app/src/androidTest/java/com/stickearn/moviefavourite/data/PopularMovieDaoTest.kt
@@ -0,0 +1,49 @@
+package com.stickearn.moviefavourite.data
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.stickearn.moviefavourite.service.database.dao.PopularMovieDetailDao
+import com.stickearn.moviefavourite.util.testMovie1
+import com.stickearn.moviefavourite.util.testMovie2
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class PopularMovieDaoTest : DbTest() {
+    private lateinit var popularMovieDetailDao: PopularMovieDetailDao
+    private val setA = testMovie1.copy()
+    private val setB = testMovie2.copy()
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun createDb() {
+        popularMovieDetailDao = db.popularMovieDetailDao()
+        runBlocking {
+            popularMovieDetailDao.insertAll(listOf(setA, setB))
+        }
+    }
+
+    @Test
+    fun testGetPopularMovie() {
+        val list = popularMovieDetailDao.getAll()
+        Assert.assertThat(list.size, Matchers.equalTo(2))
+
+        Assert.assertThat(list[0], Matchers.equalTo(setA))
+        Assert.assertThat(list[1], Matchers.equalTo(setB))
+    }
+
+    @Test
+    fun testQueryDeleteFromDao() {
+//        categoryDao.deleteData()
+//
+//        val list = categoryDao.getCategories()
+//        Assert.assertThat(list.size, Matchers.equalTo(0))
+    }
+
+}
---
+++ app/src/androidTest/java/com/stickearn/moviefavourite/util/DataUtil.kt
@@ -0,0 +1,21 @@
+package com.stickearn.moviefavourite.util
+
+import com.stickearn.moviefavourite.model.popularmovie.PopularMovieDetail
+
+val testMovie1 = PopularMovieDetail(
+    id=1,
+    backdropPath = "picture1.jpg",
+    overview = "this is overview1",
+    posterPath = "posterPath1.jpg",
+    releaseDate = "1995-08-21",
+    title = "Sebuah Jalan"
+)
+
+val testMovie2 = PopularMovieDetail(
+    id=2,
+    backdropPath = "picture2.jpg",
+    overview = "this is overview2",
+    posterPath = "posterPath2.jpg",
+    releaseDate = "1996-07-21",
+    title = "Sebuah Cerita"
+)

6e66572d7464a15414b048eb8588e889ffa4edc7
Dhaba Widhikari <wikodhawi@gmail.com>
2020-08-17 07:46:27 +0700

    - tes flow to detail movie -Check visible invisible main activity

---
+++ app/src/androidTest/java/com/stickearn/moviefavourite/view/main/activity/MainActivityTest.kt
@@ -0,0 +1,35 @@
+package com.stickearn.moviefavourite.view.main.activity
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.LargeTest
+import com.stickearn.moviefavourite.R
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun checkVisibleInvisibleMainActivity() {
+        // Please make sure your screen device on
+        Espresso.onView(withId(R.id.shimmerPopularMovie)).check(matches(isDisplayed()))
+        Espresso.onView(withId(R.id.shimmerNowPlayingMovie)).check(matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.shimmerTopRatedMovie)).check(matches(isDisplayed()))
+        Espresso.onView(withId(R.id.rcyPopularMovie)).check(matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.rcyNowPlaying)).check(matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.rcyTopRatedMovie)).check(matches(not(isDisplayed())))
+    }
+}
